2647fd9b98d9750e6bc9c2e57d33c738e0363199
Liam Townsley <liamtownsley42@gmail.com>
2021-02-08 16:23:02 +0000

    feat: added contact page & finished faq

--- scripts/support.js
+++ scripts/support.js
@@ -1,6 +1,5 @@
 function toggleChild(questionNumber) {
     const question = document.getElementById(`question${questionNumber}`);
-    const answer = document.getElementById(`answer${questionNumber}`);
     $("#answer" + questionNumber).slideToggle();
     if(question.classList.contains("active")) {
         question.classList.remove("active");
